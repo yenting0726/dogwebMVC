1ce8fbe271e086d6f26d42dbdb4332aed51aaebe
yenting0726 <linfamily14@gmail.com>
2025-07-09 11:54:49 +0800

    123
    123

--- obj/Debug/net8.0/dogwebMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/dogwebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dogwebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc438aa96c8a582631e52945262a624e9dfd054b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5752c661352cfe803b6b06136b939dcfa958836")]
 [assembly: System.Reflection.AssemblyProductAttribute("dogwebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dogwebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
